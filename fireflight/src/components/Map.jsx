d13b703b28bd34212ff3f762174ea290e8712670
ken1286 <kindle1286@gmail.com>
2019-08-22 14:36:56 -0700

    implemented popups to click on each location

--- fireflight/src/components/Map.jsx
+++ fireflight/src/components/Map.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import ReactMapGL, { Marker } from "react-map-gl";
+import React, { useState, useEffect } from "react";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
 
 const Map = props => {
   const [viewport, setViewport] = useState({
@@ -9,10 +9,37 @@ const Map = props => {
     longitude: -122.4376,
     zoom: 8
   });
+  const [selectedFire, setSelectedFire] = useState(null);
 
   const token =
     process.env.REACT_APP_MAPBOX_TOKEN ||
     "pk.eyJ1Ijoia2VuMTI4NiIsImEiOiJjanpuMXdlb2UwZzlkM2JsY2t2aTVkcGFoIn0.eGKKY2f3oC5s8GqsyB70Yg";
+
+  const dummyFireData = [
+    {
+      location: "location1",
+      latitude: 37.757,
+      longitude: -122.437
+    },
+    {
+      location: "location2",
+      latitude: 37.68,
+      longitude: -122
+    }
+  ];
+
+  useEffect(() => {
+    const listener = e => {
+      if (e.key === "Escape") {
+        setSelectedFire(null);
+      }
+    };
+    window.addEventListener("keydown", listener);
+
+    return () => {
+      window.removeEventListener("keydown", listener);
+    };
+  }, []);
 
   return (
     <div>
@@ -23,17 +50,36 @@ const Map = props => {
           setViewport(viewport);
         }}
       >
-        <div>
-          marker data here, example below
-          {/* {fireData.map(fire => { */}
-          <Marker latitude={37.757} longitude={-122.437}>
-            <div>FIRE</div>
-          </Marker>
+        marker data here, example below
+        {dummyFireData.map(fire => {
+          return (
+            <Marker latitude={fire.latitude} longitude={fire.longitude}>
+              <button
+                style={{ width: "20px", height: "15px" }}
+                onClick={e => {
+                  e.preventDefault();
+                  setSelectedFire(fire);
+                }}
+              />
+              FIRE{" "}
+            </Marker>
+          );
+        })}
+        {/*
           <Marker latitude={37.68} longitude={-122}>
-            <div>FIRE</div>
-          </Marker>
-          ;{/* })} */}
-        </div>
+            <button>FIRE</button>
+          </Marker> */}
+        {selectedFire ? (
+          <Popup
+            latitude={selectedFire.latitude}
+            longitude={selectedFire.longitude}
+            onClose={() => {
+              setSelectedFire(null);
+            }}
+          >
+            <div>{selectedFire.location}</div>
+          </Popup>
+        ) : null}
       </ReactMapGL>
     </div>
   );
