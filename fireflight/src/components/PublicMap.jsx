535acae8dbe1cb598ded27799723eb9446333017
Michael Mangus <mjmangus16@yahoo.com>
2019-08-28 22:05:15 -0400

    added an address search bar to the publicMap

--- fireflight/src/components/PublicMap.jsx
+++ fireflight/src/components/PublicMap.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useContext } from "react";
 import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import styled from "styled-components";
 
 import fireIcon from "../images/fireIcon.png";
 import locationIcon from "../images/locationIcon.png";
@@ -26,9 +27,34 @@ const PublicMap = () => {
         onViewportChange={viewport => {
           setViewport(viewport);
         }}
-      ></ReactMapGL>
+      >
+        <FormContainer>
+          <FormInput
+            type="text"
+            name="Search_Address"
+            placeholder="Search Address"
+          />
+        </FormContainer>
+      </ReactMapGL>
     </div>
   );
 };
 
 export default PublicMap;
+
+const FormContainer = styled.form`
+  display: flex;
+`;
+
+const FormInput = styled.input`
+  width: 300px;
+  margin: 25px auto;
+  padding: 15px;
+  font-size: 0.75em;
+  background-color: white;
+  border-radius: 5px;
+  border: solid 1px black;
+  @media (max-width: 900px) {
+    width: 75%;
+  }
+`;
