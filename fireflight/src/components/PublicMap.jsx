5a31f2933e5116c7ef29d2535e1d59c2b42a6c0b
Michael Mangus <mjmangus16@yahoo.com>
2019-08-29 15:53:40 -0400

    home page is fully functional and shows users location along with active fires in area

--- fireflight/src/components/PublicMap.jsx
+++ fireflight/src/components/PublicMap.jsx
@@ -13,15 +13,54 @@ const token =
   "pk.eyJ1Ijoia2VuMTI4NiIsImEiOiJjanpuMXdlb2UwZzlkM2JsY2t2aTVkcGFoIn0.eGKKY2f3oC5s8GqsyB70Yg";
 
 const PublicMap = () => {
-  const { publicMapState, setViewport, setAddress } = useContext(
+  const { publicMapState, setViewport, setAddress, getData } = useContext(
     PublicMapContext
   );
-  const { viewport, address } = publicMapState;
+  const { viewport, address, coordinates, fireData } = publicMapState;
 
-  console.log(address);
+  const handleSubmit = () => {
+    getData();
+  };
+
+  let userMarker;
+
+  if (coordinates.latitude && coordinates.longitude) {
+    userMarker = (
+      <Marker latitude={coordinates.latitude} longitude={coordinates.longitude}>
+        <img src={locationIcon} height="35" width="20" style={{ zIndex: -1 }} />
+      </Marker>
+    );
+  }
+
+  let activeFires = [];
+
+  if (fireData.length > 0) {
+    activeFires = fireData.map(fire => {
+      return (
+        // return marker for each fire datapoint
+        <Marker
+          latitude={fire.latitude}
+          longitude={fire.longitude}
+          key={`${fire.distance}_${fire.latitude}`}
+        >
+          <img src={fireIcon} height="35" width="35" style={{ zIndex: 3 }} />
+        </Marker>
+      );
+    });
+  }
 
   return (
-    <div>
+    <div style={{ position: "relative" }}>
+      <FormContainer>
+        <FormInput
+          type="text"
+          name="Search Address"
+          placeholder="Search Address"
+          value={address}
+          onChange={e => setAddress(e.target.value)}
+        />
+        <FormButton onClick={handleSubmit}>SUBMIT</FormButton>
+      </FormContainer>
       <ReactMapGL
         {...viewport}
         mapboxApiAccessToken={token}
@@ -29,15 +68,8 @@ const PublicMap = () => {
           setViewport(viewport);
         }}
       >
-        <FormContainer>
-          <FormInput
-            type="text"
-            name="Search_Address"
-            placeholder="Search Address"
-            value={address}
-            onChange={e => setAddress(e.target.value)}
-          />
-        </FormContainer>
+        {userMarker}
+        {activeFires}
       </ReactMapGL>
     </div>
   );
@@ -45,13 +77,19 @@ const PublicMap = () => {
 
 export default PublicMap;
 
-const FormContainer = styled.form`
+const FormContainer = styled.div`
+  position: absolute;
   display: flex;
+  justify-content: center;
+  z-index: 10;
+  left: 0;
+  right: 0;
+  margin: auto;
 `;
 
 const FormInput = styled.input`
   width: 300px;
-  margin: 25px auto;
+  margin: 25px;
   padding: 15px;
   font-size: 0.75em;
   background-color: white;
@@ -61,3 +99,5 @@ const FormInput = styled.input`
     width: 75%;
   }
 `;
+
+const FormButton = styled.button``;
--- fireflight/src/context/types.js
+++ fireflight/src/context/types.js
@@ -6,3 +6,5 @@ export const SET_COORDS = "SET_COORDS";
 export const GET_FIRES_START = "GET_FIRES_START";
 export const GET_FIRES_SUCCESS = "GET_FIRES_SUCCESS";
 export const GET_FIRES_ERROR = "GET_FIRES_ERROR";
+export const SET_FIRE_DATA = "SET_FIRE_DATA";
+export const SET_COORDINATES = "SET_COORDINATES";
