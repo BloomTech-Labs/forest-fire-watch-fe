5bf133f54f83f238b80d51ddfb6c6de7551f627b
Michael Mangus <mjmangus16@yahoo.com>
2019-09-03 13:44:21 -0400

    User's are being alerted if there is an active fire near any of their added locations.

--- fireflight/src/components/Alerts.jsx
+++ fireflight/src/components/Alerts.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useContext } from "react";
 
 import { AlertContext } from "../context/AlertContext";
+import styled from "styled-components";
 
 const Alerts = () => {
   const { alertState, getCoords } = useContext(AlertContext);
@@ -16,19 +17,35 @@ const Alerts = () => {
 
   if (fireData.length > 0) {
     myAlerts = (
-      <div>
-        {/* {fireData.forEach(data => (
-        <h4>{data.address}</h4>
-        
-      ))} */}
-        There are active fires in your area
-      </div>
+      <AlertContainer>
+        <AlertHeading>
+          There are active fires near the following locations...
+        </AlertHeading>
+        {fireData.map(loc => (
+          <LocAddress key={loc.address}>{loc.address}</LocAddress>
+        ))}
+        <h5>Please check that map for further details</h5>
+      </AlertContainer>
     );
   } else {
-    myAlerts = <p>There are no active wildfires in your area.</p>;
+    myAlerts = (
+      <AlertContainer>
+        <AlertHeading>There are no active wildfires in your area</AlertHeading>
+      </AlertContainer>
+    );
   }
 
   return <div>{myAlerts}</div>;
 };
 
 export default Alerts;
+
+const AlertContainer = styled.div`
+  text-align: center;
+`;
+
+const AlertHeading = styled.h3``;
+
+const LocAddress = styled.h4`
+  text-decoration: underline;
+`;
