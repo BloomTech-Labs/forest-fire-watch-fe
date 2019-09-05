e6c8f2663ce9ddc6e7a2bf8dadd886ea5e1a3edf
Michael Mangus <mjmangus16@yahoo.com>
2019-09-05 11:09:52 -0400

    added an alert icon to the alert modal

--- fireflight/src/components/Alerts.jsx
+++ fireflight/src/components/Alerts.jsx
@@ -3,6 +3,8 @@ import React, { useState, useEffect, useContext } from "react";
 import { FireDataContext } from "../context/FireDataContext";
 import styled from "styled-components";
 
+import alertIcon from "../images/alert.png";
+
 const Alerts = () => {
   const { fireDataState } = useContext(FireDataContext);
   const { alertData } = fireDataState;
@@ -12,6 +14,7 @@ const Alerts = () => {
   if (alertData.length > 0) {
     myAlerts = (
       <AlertContainer>
+        <img src={alertIcon} width="100px" />
         <AlertHeading>
           There are active fires near the following locations...
         </AlertHeading>
@@ -36,9 +39,13 @@ export default Alerts;
 
 const AlertContainer = styled.div`
   text-align: center;
+  background-color: #f2f3f4;
+  border-radius: 8px;
 `;
 
-const AlertHeading = styled.h3``;
+const AlertHeading = styled.h3`
+  color: #e61a1a;
+`;
 
 const LocAddress = styled.h4`
   text-decoration: underline;
