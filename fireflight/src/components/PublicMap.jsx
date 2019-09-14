cce7e08b2e2bb5a5aa634a83efc7b27d4440eff8
Michael Mangus <mjmangus16@yahoo.com>
2019-09-14 08:50:07 -0400

    added toggle switch

--- fireflight/src/components/PublicMap.jsx
+++ fireflight/src/components/PublicMap.jsx
@@ -76,11 +76,10 @@ const PublicMap = ({ setShowAuth, setShowLogin, setShowRegister }) => {
   const savedLocationPopup = (
     <div style={{ display: "flex", flexDirection: "column" }}>
       {/* <button style={{ marginBottom: 6 }}>Toggle Notifications</button> */}
-      <Switch>
-        <Checkbox type="checkbox" />
-
-        <Slider></Slider>
-      </Switch>
+      <CheckBoxWrapper>
+        <CheckBox id="checkbox" type="checkbox" />
+        <CheckBoxLabel htmlFor="checkbox" />
+      </CheckBoxWrapper>
       <button style={{ marginTop: 6 }}>Delete this pin</button>
     </div>
   );
@@ -250,55 +249,46 @@ const PopupText = styled.p`
   margin: 0px;
 `;
 
-const Switch = styled.label`
+const CheckBoxWrapper = styled.div`
   position: relative;
-  display: inline-block;
-  width: 60px;
-  height: 34px;
 `;
-
-const Checkbox = styled.input`
-  /* opacity: 0; */
-  width: 0;
-  height: 0;
-  &:checked {
-    background-color: #2196f3;
-    -webkit-transform: translateX(26px);
-    -ms-transform: translateX(26px);
-    transform: translateX(26px);
-  }
-  &:focus {
-    box-shadow: 0 0 1px #2196f3;
-  }
-`;
-
-const Slider = styled.span`
+const CheckBoxLabel = styled.label`
   position: absolute;
-  cursor: pointer;
   top: 0;
   left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: #ccc;
-  /* -webkit-transition: 0.4s; */
-  /* transition: 0.4s; */
-  background-color: #2196f3;
-  box-shadow: 0 0 1px #2196f3;
-  border-radius: 34px;
-
-  &:before {
-    position: absolute;
+  width: 42px;
+  height: 26px;
+  border-radius: 15px;
+  background: #bebebe;
+  cursor: pointer;
+  &::after {
     content: "";
-    height: 26px;
-    width: 26px;
-    left: 4px;
-    bottom: 4px;
-    background-color: white;
-    /* -webkit-transition: 0.4s;
-    transition: 0.4s;
-    -webkit-transform: translateX(26px);
-    -ms-transform: translateX(26px); */
-    transform: translateX(26px);
+    display: block;
     border-radius: 50%;
+    width: 18px;
+    height: 18px;
+    margin: 3px;
+    background: #ffffff;
+    box-shadow: 1px 3px 3px 1px rgba(0, 0, 0, 0.2);
+    transition: 0.2s;
+  }
+`;
+const CheckBox = styled.input`
+  opacity: 0;
+  z-index: 1;
+  border-radius: 15px;
+  width: 42px;
+  height: 26px;
+  &:checked + ${CheckBoxLabel} {
+    background: #4fbe79;
+    &::after {
+      content: "";
+      display: block;
+      border-radius: 50%;
+      width: 18px;
+      height: 18px;
+      margin-left: 21px;
+      transition: 0.2s;
+    }
   }
 `;
