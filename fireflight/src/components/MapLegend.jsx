ae436fc96cc5e0936b9079545f49cc90dad2d7f6
Michael Mangus <mjmangus16@yahoo.com>
2019-09-22 08:35:22 -0400

    basic map legend is complete

--- fireflight/src/components/MapLegend.jsx
+++ fireflight/src/components/MapLegend.jsx
@@ -9,40 +9,62 @@ import locationIconGreen from "../images/locationIconGreen.png";
 const MapLegend = () => {
   return (
     <LegendContainer>
-      <h4 style={{ textAlign: "center", textDecoration: "underline" }}>
+      <h4
+        style={{
+          textAlign: "center",
+          textDecoration: "underline",
+          margin: 10
+        }}
+      >
         Map Legend
       </h4>
       <div style={{ display: "flex", flexDirection: "column" }}>
         <LegendItem>
-          <h5>Temp. Location Marker:</h5>
+          <LegendText>Temp. Location Marker:</LegendText>
           <img
             src={locationIcon}
-            height="35"
-            width="20"
+            height="25"
+            width="15"
             style={{ zIndex: 5, transform: "translate(0px, 10px)" }}
             alt=""
           />
         </LegendItem>
         <LegendItem>
-          <h5>Saved Location Marker:</h5>
+          <LegendText>Saved Location Marker:</LegendText>
           <img
             src={locationIconGreen}
-            height="35"
-            width="20"
+            height="25"
+            width="15"
             style={{ zIndex: 5, transform: "translate(0px, 10px)" }}
             alt=""
           />
         </LegendItem>
         <LegendItem>
-          <h5>Wildfire Marker:</h5>
+          <LegendText>Wildfire Marker:</LegendText>
           <img
             src={fireIcon}
-            height="35"
-            width="35"
+            height="20"
+            width="15"
             style={{ zIndex: 5, transform: "translate(0px, 10px)" }}
             alt=""
           />
         </LegendItem>
+        <LegendItem>
+          <LegendText>Fire Within Radius:</LegendText>
+          <img
+            src={exclamationMark}
+            height="20"
+            width="15"
+            style={{ zIndex: 5, transform: "translate(0px, 10px)" }}
+            alt=""
+          />
+        </LegendItem>
+        <Info>
+          Location markers can be clicked. There are actions available on the
+          Temp. Location Marker that will allow you to save that location to
+          your profile. Once saved, you can choose to receive alerts for that
+          location and adjust the alert radius.
+        </Info>
       </div>
     </LegendContainer>
   );
@@ -67,4 +89,16 @@ const LegendItem = styled.span`
   margin: auto;
   display: flex;
   justify-content: space-between;
+`;
+
+const LegendText = styled.h5`
+  margin: 15px;
+`;
+
+const Info = styled.p`
+  width: 90%;
+  margin: 10px auto;
+  text-align: center;
+  font-size: 0.75rem;
+  font-weight: 300;
 `;
