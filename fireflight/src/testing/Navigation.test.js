9883da8f2d36614feec6e045b93606ec73c58cda
Penny Tague <pmtague9@gmail.com>
2019-11-12 13:51:44 -0800

    Updated test for text rendered on home page to reflect new text

--- fireflight/src/testing/Navigation.test.js
+++ fireflight/src/testing/Navigation.test.js
@@ -1,8 +1,11 @@
 import React from "react";
 import Navigation from "../components/Navigation";
+import Login from "../components/AuthForms/Login";
+import Modal from "../components/AuthForms/Combine";
 import { render, fireEvent } from "./test-utils";
 import { createMemoryHistory } from 'history'
 import fire from "../config/fire";
+import expectExport from "expect";
 
 describe("<Navigation />", () => {
 	it("renders without crashing", () => {
@@ -14,11 +17,12 @@ describe("<Navigation />", () => {
 	    
 		nav.getByText(/Home/i)
 	})
-	it("displays login modal when clicking Sign In", () => {
+	it("displays login modal when clicking Sign In", async () => {
 		const { getByText, findByText } = render(<Navigation />)
+		const login = render(<Modal><Login /></Modal>)
 
-		fireEvent.click(getByText('Sign In'))
+		await fireEvent.click(getByText('Sign In'))
 
-		
+		login.getByText(/Welcome Back/i);
 	})
 });
