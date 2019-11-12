8b76f16bac599d5b4c98c3451de93a73a2be34fe
Neha Patel <neha.r.p@gmail.com>
2019-11-12 14:00:18 -0800

    grabbed Sign In button successfully

---
+++ fireflight/cypress/integration/app_test.spec.js
@@ -0,0 +1,8 @@
+describe('Loads the sign up modal', function() {
+  it('loaded the staging URl', function() {
+    cy.visit('http://wildfire-watch-staging.netlify.com')
+    cy.get('.menu-item')
+      .contains('Sign In')
+      .click()
+  })
+})
