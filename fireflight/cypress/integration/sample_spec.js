4c3cf995496bb50050b6c518e9162e9cfe5f0620
astillo <clomes.alex@gmail.com>
2019-11-12 16:55:08 -0600

    Delete sample_spec.js

--- fireflight/cypress/integration/sample_spec.js
+++
@@ -1,6 +0,0 @@
-describe('Loads the sign up modal', function() {
-  it('loaded the staging URl', function() {
-    cy.visit('http://wildfire-watch-staging.netlify.com')
-    cy.contains('Sign In').click()
-  })
-})
