586f346adc3d266217445b2574d0b9b393594f2a
Neha Patel <neha.r.p@gmail.com>
2019-11-12 15:37:25 -0800

    test to switch to register modal from login modal

--- fireflight/cypress/integration/app_test.spec.js
+++ fireflight/cypress/integration/app_test.spec.js
@@ -1,10 +1,20 @@
-describe('Loads the sign up modal', function() {
-  it('loaded the staging URl', function() {
+describe('Wildfire Watch App', function() {
+  it('loaded the staging URL', function() {
     cy.visit('http://wildfire-watch-staging.netlify.com')
+  })
+  
+  it('loads the login modal upon clicking "sign in"', () => {
     cy.get('.menu-item')
       .contains('Sign In')
       .click()
 
-    cy.get('.model-wrapper').should('have.css','opacity', '1')
+    cy.get('.model-wrapper').should('have.css', 'opacity', '1')
+    cy.get('.form-heading').should('have.text', 'Welcome Back')
+  })
+
+  it('Switches to register modal from login', () => {
+    cy.get('.create-an-account').click()
+    cy.get('.register-page-container').should('be.visible')
+    cy.get('.form-heading').should('have.text', 'Create an Account')
   })
 })
