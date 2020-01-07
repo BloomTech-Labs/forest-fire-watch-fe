0f41ae4291834661cfbf732c98449753475e000f
chrismjohnston <cmjohnston@gmail.com>
2020-01-07 15:59:07 -0600

    had to reclone and start over

--- fireflight/src/components/HamburgerNavigation.jsx
+++ fireflight/src/components/HamburgerNavigation.jsx
@@ -20,7 +20,7 @@ import ListItemText from '@material-ui/core/ListItemText'
 import MapLegend from '../components/MapLegend'
 import { grey } from '@material-ui/core/colors'
 
-const drawerWidth = 240
+const drawerWidth = 140
 
 const useStyles = makeStyles(theme => ({
   root: {
