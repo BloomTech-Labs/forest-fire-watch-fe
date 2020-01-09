4ff14588c03c005f89bf5e70924e9e33b507ee98
Kenna Lawrie <kenna@Kennas-MBP.attlocal.net>
2020-01-09 13:00:21 -0500

    deleted table cells for additional columns

--- fireflight/src/components/CheckList.jsx
+++ fireflight/src/components/CheckList.jsx
@@ -283,10 +283,6 @@ export default function EnhancedTable() {
                       <TableCell component="th" id={labelId} scope="row" padding="none">
                         {row.name}
                       </TableCell>
-                      <TableCell align="right">{row.calories}</TableCell>
-                      <TableCell align="right">{row.fat}</TableCell>
-                      <TableCell align="right">{row.carbs}</TableCell>
-                      <TableCell align="right">{row.protein}</TableCell>
                     </TableRow>
                   );
                 })}
