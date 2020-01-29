e88a3f5306c57c2813ac00c1ac1a5dfcaa220820
kenna <kennaalastair@gmail.com>
2020-01-28 17:48:44 -0800

    border on last span element removed

--- fireflight/src/components/ColorLegend.jsx
+++ fireflight/src/components/ColorLegend.jsx
@@ -5,47 +5,48 @@ const ColorLegend = () => {
   const legendStyle = {
     display: 'flex',
     flexDirection: 'row',
-    marginTop: '100px',
     position: 'fixed',
     bottom: '50px',
     right: '30px',
   }
 
-  const pStyle = {
+  const spanStyle = {
     display: 'flex',
     justifyContent: 'center',
     margin: '0',
-    width: '25px'
+    width: '25px',
+    borderRight: '1px solid #fff',
+    padding: '0 15px',
   }
 
   return (
     <div className="color-legend" style={legendStyle}>
       <div id="cl-1" className="legend-item" style={{backgroundColor: "black", color: "white"}}>
-        <p style={pStyle}>N/A</p>
+        <span style={spanStyle}>N/A</span>
       </div>
       <div id="cl-2" className="legend-item" style={{backgroundColor: "green"}}>
-        <p style={pStyle}>0</p>
+        <span style={spanStyle}>0</span>
       </div>
       <div id="cl-3" className="legend-item" style={{backgroundColor: "#88DB57"}}>
-        <p style={pStyle}>25</p>
+        <span style={spanStyle}>25</span>
       </div>
       <div id="cl-4" className="legend-item" style={{backgroundColor: "#FFF072"}}>
-        <p style={pStyle}>50</p>
+        <span style={spanStyle}>50</span>
       </div>
       <div id="cl-5" className="legend-item" style={{backgroundColor: "yellow"}}>
-        <p style={pStyle}>75</p>
+        <span style={spanStyle}>75</span>
       </div>
       <div id="cl-6" className="legend-item" style={{backgroundColor: "darkorange"}}>
-        <p style={pStyle}>100</p>
+        <span style={spanStyle}>100</span>
       </div>
       <div id="cl-7" className="legend-item" style={{backgroundColor: "red", color: "white"}}>
-        <p style={pStyle}>200</p>
+        <span style={spanStyle}>200</span>
       </div>
       <div id="cl-8" className="legend-item" style={{backgroundColor: "darkred", color: "white"}}>
-        <p style={pStyle}>300</p>
+        <span style={spanStyle}>300</span>
       </div>
-      <div id="cl-9" className="legend-item" style={{backgroundColor: "#660000", color: "white"}}>
-        <p style={pStyle}>500</p>
+      <div id="cl-9" className="legend-item" style={{backgroundColor: "#600", color: "white"}}>
+        <span style={{spanStyle}, {borderRight: 'none', padding: '0 5px'}}>500</span>
       </div>
     </div>
   )
