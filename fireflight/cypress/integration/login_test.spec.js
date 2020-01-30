dd9069eaa52e4c98a5196a54a85f79fb03a314c9
Lisa Campbell <lisa@Lisas-MBP.lan>
2020-01-30 08:39:01 -0800

    Forgot to create new branch.

--- fireflight/cypress/integration/login_test.spec.js
+++ fireflight/cypress/integration/login_test.spec.js
@@ -9,21 +9,57 @@ describe('login form', () => {
     it('greets with Welcome Back', () => {
         cy.contains('h2', "Welcome Back")
     })
-    // checks for email error message after clicking the Sign In button
+    //checks for email error message after clicking the Sign In button
     it('requires email', () => {
         cy.get('[name=password]').type('testing123')
         cy.get('[type=submit]').click()
         cy.contains('The email address is badly formatted.')
     })
-    // checks for missing password error message after hitting the enter key
+    //checks for missing password error message after hitting the enter key
     it('requires password', () => {
         cy.get('[name=email]').type('cypress@newemail.com{enter}')
         cy.contains('The password is invalid or the user does not have a password.')
     })
-    // checks for error message when email address is not associated with an account
+    //checks for error message when email address is not associated with an account
     it('produce error message when there is no account', () => {
         cy.get('[name=email]').type('press@testing.com')
         cy.get('[name=password]').type('testing123{enter}')
         cy.contains('There is no user record corresponding to this identifier. The user may have been deleted.')
     })
 })
+
+// describe('login form', () => {
+//     beforeEach(() => {
+//         cy.visit('/')
+//         cy.get('.MuiPaper-root .MuiAppBar-root .MuiAppBar-positionFixed ')
+//             .contains('.MuiButtonBase-root .MuiIconButton-root .jss5 .MuiIconButton-colorSecondary .MuiIconButton-edgeStart .MuiIconButton-root ')
+//             .click()
+//     })
+
+//     // it('greets with Welcome Back', () => {
+//     //     cy.contains('h2', "Welcome Back")
+//     // })
+//     // checks for email error message after clicking the Sign In button
+//     // it('requires email', () => {
+//     //     cy.get('[name=password]').type('testing123')
+//     //     cy.get('[type=submit]').click()
+//     //     cy.contains('The email address is badly formatted.')
+//     // })
+//     // checks for missing password error message after hitting the enter key
+//     // it('requires password', () => {
+//     //     cy.get('[name=email]').type('cypress@newemail.com{enter}')
+//     //     cy.contains('The password is invalid or the user does not have a password.')
+//     // })
+//     // checks for error message when email address is not associated with an account
+//     it('produce error message when there is no account', () => {
+//         cy.get('[name=email]').type('press@testing.com')
+//         cy.get('[name=password]').type('testing123{enter}')
+//         cy.contains('There is no user record corresponding to this identifier. The user may have been deleted.')
+//     })
+// })
+
+// describe('My First Test', function() {
+//   it('Visits the Kitchen Sink', function() {
+//     cy.visit('https://example.cypress.io')
+//   })
+// })
