51b95f600832841cb072784898276b655dadbd72
Lisa Campbell <lisa@Lisas-MBP.lan>
2020-02-01 16:14:53 -0800

    Checklist test.

--- fireflight/cypress/integration/checklist_test.spec.js
+++ fireflight/cypress/integration/checklist_test.spec.js
@@ -5,11 +5,27 @@ describe('goes to Checklist', () => {
       .click({ multiple: true })
   })
 
-  it('Navigates to the "Checklist" when clicked and checks all boxes', () => {
-      cy.get('.MuiListItemText-root')
-        .contains('Checklist') 
-        .click({ force: true })
-      cy.location('pathname').should('eq', '/checklist')
-      cy.get('[type="checkbox"]').check()
+  it('Navigates to the "Checklist" when clicked and checks all boxes, navigates away and back. Returns to all boxes still checked', () => {
+    cy.get('.MuiButtonBase-root.MuiIconButton-root.jss5')
+      .click()
+    cy.get('.MuiListItemText-root')
+      .contains('Checklist')
+      .click() 
+    cy.location('pathname').should('eq', '/checklist')
+    cy.get('[type="checkbox"]').check()
+    cy.get('.MuiButtonBase-root.MuiIconButton-root.jss5')
+      .click()
+    cy.get('.MuiListItemText-root')
+      .contains('Home')
+      .click()
+    cy.location('pathname').should('eq', '/')
+    cy.get('.MuiButtonBase-root.MuiIconButton-root.jss5')
+      .click()
+    cy.get('.MuiListItemText-root')
+      .contains('Checklist')
+      .click() 
+    cy.location('pathname').should('eq', '/checklist')
   })
+
+  
 })
